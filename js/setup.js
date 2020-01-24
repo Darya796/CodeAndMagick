276f8702e6363224b2cdeb23b5e6e4e9e011f5ca
Darya796 <dashutkadevi@gmail.com>
2020-01-24 14:46:33 +0200

    add dialog dragging from hw in 5 lesson

--- js/setup.js
+++ js/setup.js
@@ -112,6 +112,8 @@ var openPopup = function () {
 
 var closePopup = function () {
     userDialog.classList.add("hidden");
+    userDialog.style.top = "";
+    userDialog.style.left = "";
     document.removeEventListener("keydown", onPopupEscPress);
 };
 
@@ -134,6 +136,59 @@ setupClose.addEventListener("keydown", function (evt) {
         closePopup();
     }
 });
+
+
+/*------------------------------------------------------------------------------------------------------------------*/
+var dialogHandler = userDialog.querySelector(".upload");
+
+dialogHandler.addEventListener("mousedown", function (evt) {
+    evt.preventDefault();
+
+    var startCoords = {
+        x: evt.clientX,
+        y: evt.clientY
+    };
+
+    var dragged = false;
+
+    var onMouseMove = function (moveEvt) {
+        moveEvt.preventDefault();
+        dragged = true;
+
+        var shift = {
+            x: startCoords.x - moveEvt.clientX,
+            y: startCoords.y - moveEvt.clientY
+        };
+
+        startCoords = {
+            x: moveEvt.clientX,
+            y: moveEvt.clientY
+        };
+
+        userDialog.style.top = (userDialog.offsetTop - shift.y) + "px";
+        userDialog.style.left = (userDialog.offsetLeft - shift.x) + "px";
+    };
+
+    var onMouseUp = function (upEvt) {
+        upEvt.preventDefault();
+
+        document.removeEventListener("mousemove", onMouseMove);
+        document.removeEventListener("mouseup", onMouseUp);
+
+        if(dragged) {
+            var onClickPreventDefault = function (evt) {
+                evt.preventDefault();
+                dialogHandler.removeEventListener("click", onClickPreventDefault)
+            };
+            dialogHandler.addEventListener("click", onClickPreventDefault);
+        }
+    };
+
+    document.addEventListener("mousemove", onMouseMove);
+    document.addEventListener("mouseup", onMouseUp);
+});
+/*------------------------------------------------------------------------------------------------------------------*/
+
 
 userNameInput.addEventListener("invalid", function (evt) {
     if (userNameInput.validity.tooShort) {
